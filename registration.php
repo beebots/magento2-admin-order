e0c67c052d7a6efbe94864cc6c2dbe0d0e687de5
Scott Friend <scottn@banyanbotanicals.com>
2020-01-15 12:52:11 -0800

    Renaming bbots to beebots

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'BBots_AdminOrder',
+    'BeeBots_AdminOrder',
     __DIR__
 );
