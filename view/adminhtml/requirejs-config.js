2f8234813328b8cfbb566f3defa0ca99f81a76b6
shilpam <5776197+shilpsauce@users.noreply.github.com>
2020-02-27 11:03:36 -0600

    move order-reload-helper to another project

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -6,7 +6,6 @@ var config = {
             'selectize': 'BeeBots_AdminOrder/js/lib/selectize',
             'order-item-adder': 'BeeBots_AdminOrder/js/order-item-adder',
             'order-item-actions': 'BeeBots_AdminOrder/js/order-item-actions',
-            'order-reload-helper': 'BeeBots_AdminOrder/js/order-reload-helper',
             'order-coupon': 'BeeBots_AdminOrder/js/order-coupon',
             'order-item-price-percentage': 'BeeBots_AdminOrder/js/order-item-price-percentage',
             'order-tabs-ui': "BeeBots_AdminOrder/js/order-tabs-ui",
--- view/adminhtml/web/js/order-save-address.js
+++ view/adminhtml/web/js/order-save-address.js
@@ -7,37 +7,45 @@ define([
 
     return {
         init: function(){
-            orderReloadHelper.initReloadCallback('order-shipping_address', this._initShipping.bind(this));
             this._initBilling();
             this._initShipping();
+            this._initShippingReload();
+            orderReloadHelper.onReloadAreas('beeSaveAddressBookShipping', this._initShipping.bind(this));
+            orderReloadHelper.onReloadAreas('beeSaveAddressBookShippingReload', this._initShippingReload.bind(this));
+            orderReloadHelper.onReloadAreas('beeSaveAddressBookBilling', this._initBilling.bind(this));
         },
 
         _initBilling: function(){
+            console.log('orderSaveAddress initBillingAddress');
             this.onBillingAddressChange();
             $jQuery('#order-billing_address_customer_address_id').change(this.onBillingAddressChange.bind(this));
         },
 
         _initShipping: function(){
+            console.log('orderSaveAddress initShippingAddress');
             this.onShippingAddressChange();
             $jQuery('#order-shipping_address_customer_address_id').change(this.onShippingAddressChange.bind(this));
         },
 
+        _initShippingReload: function(){
+            orderReloadHelper.initAreaReloadCallback('beeSaveAddressBookInit', 'order-shipping_address', this._initShipping.bind(this));
+        },
+
         onBillingAddressChange(){
-            debugger;
             let billingAddress = $jQuery('#order-billing_address_customer_address_id').val();
             if(billingAddress){
-                $jQuery('#order-billing_address_save_in_address_book').attr('checked', null);
+                $jQuery('#order-billing_address_save_in_address_book').prop('checked', null);
             } else {
-                $jQuery('#order-billing_address_save_in_address_book').attr('checked', 'checked');
+                $jQuery('#order-billing_address_save_in_address_book').prop('checked', 'checked');
             }
         },
 
         onShippingAddressChange(){
             let shippingAddress = $jQuery('#order-shipping_address_customer_address_id').val();
             if(shippingAddress){
-                $jQuery('#order-shipping_address_save_in_address_book').attr('checked', null);
+                $jQuery('#order-shipping_address_save_in_address_book').prop('checked', null);
             } else {
-                $jQuery('#order-shipping_address_save_in_address_book').attr('checked', 'checked');
+                $jQuery('#order-shipping_address_save_in_address_book').prop('checked', 'checked');
             }
         }
     }
