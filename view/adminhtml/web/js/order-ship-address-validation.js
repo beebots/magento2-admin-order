f412c0dda29d97a58dfdd04a0fa5f76515140d3d
Scott Friend <scottn@banyanbotanicals.com>
2021-02-19 15:51:06 -0800

    Adding better support for address validation when billing = shipping

--- view/adminhtml/web/js/order-ship-address-validation.js
+++ view/adminhtml/web/js/order-ship-address-validation.js
@@ -5,6 +5,8 @@ define([
 ], function ($, orderTabsUi) {
 
     let shippingFieldsSelector = '#order-shipping_address input:not([type="checkbox"]), #order-shipping_address select';
+    let billingFieldsSelector = '#order-billing_address input:not([type="checkbox"]), #order-billing_address select';
+    let sameAsBillingId = 'order-shipping_same_as_billing';
 
     return {
 
@@ -23,13 +25,21 @@ define([
             let $validator = $form.validate();
             let valid = true;
             let firstInvalid = null;
-            $(shippingFieldsSelector).each(function () {
+            let useBillingForShipping = document.getElementById(sameAsBillingId)
+                && document.getElementById(sameAsBillingId).checked;
+
+            let fieldsToValidateSelector = useBillingForShipping
+                ? billingFieldsSelector
+                : shippingFieldsSelector;
+
+            $(fieldsToValidateSelector).each(function () {
                 let result = $validator.element(this);
                 if(!result && !firstInvalid){
                     firstInvalid = this;
                 }
                 valid &= result;
             });
+            
             if(!valid){
                 this.selectAddressTab();
                 $validator.element(firstInvalid);
